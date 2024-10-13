ba702b8cbc2ea3e1827b4b26358ba993b5355729
FairusKN <kamalnapidfairus@gmail.com>
2024-10-13 13:59:24 +0700

    Removed old ryo_dotfile directory

--- ryo_dotfile/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d9d5dc", "#0c0e1a" },
-	[SchemeSel] = { "#d9d5dc", "#4361AD" },
-	[SchemeOut] = { "#d9d5dc", "#9CAADD" },
-};
--- ryo_dotfile/waybar/完全备份/script/brightctl.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-
-# brightness=$(brightnessctl -m | awk -F, '{print substr($4, 1, length($4)-1)}' | sed 's/%//')
-# echo "{\"percentage\": $brightness, \"class\": \"custom-$brightness\"}"
-#!/bin/bash
-
-brightness=$(brightnessctl -m | awk -F, '{print substr($4, 1, length($4)-1)}' | sed 's/%//')
-echo "{\"percentage\": $brightness, \"tooltip\": \"$brightness\", \"class\": \"custom-$brightness\"}"
